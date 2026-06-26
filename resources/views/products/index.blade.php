587dde7ea0d1d620d0109223024ea898e150d153
Syariffullah <dinnereatc@gmail.com>
2026-06-26 08:45:06 +0700

    feat: Add pagination and footer components

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,26 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-2">
+        <div class="mt-6 flex justify-between items-center">
+            <h2 class="font-semibold text-xl">List Products</h2>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Ini Products
+            <button class="bg-gray-100 px-10 py-2 rounded-md font-semibold">Tambah</button>
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-3 mt-4 gap-6">
+            @foreach ($products as $product)
+                <div class="">
+                    <img src="{{ url('storage/' . $product->foto) }}" alt="product">
+                    <div class="my-2">
+                        <p class="text-xl font-light">{{ $product->nama }}</p>
+                        <p class="font-semibold text-gray-400">Rp {{ number_format($product->harga) }}</p>
+                    </div>
+                    <button class="bg-gray-100 px-10 py-2 rounded-md w-full font-semibold">Edit</button>
                 </div>
-            </div>
+            @endforeach
+        </div>
+
+        <div class="mt-4">
+            {{ $products->links() }}
         </div>
     </div>
 </x-app-layout>
